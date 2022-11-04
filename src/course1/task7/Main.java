1b4d87b0c10109e6991e95112be732b15f4d8fd9
Alena1308 <Lenamironova13@mail.ru>
2022-11-04 20:48:25 +0300

    finish task 7

--- src/course1/task7/Main.java
+++ src/course1/task7/Main.java
@@ -1,2 +1,11 @@
-package course1.task7;public class Main {
+package course1.task7;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(squareNumber(a));
+    }
+    public static int squareNumber (int number) {
+        return number *= number;
+    }
 }
